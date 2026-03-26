38487b44391f2413d6a7276f1d81e50d02d2f72f
DolagovindaSahu <sahulucky990@gmail.com>
2026-03-26 11:42:43 +0530

    Added get_csv_data function to the main.py file

--- csv_fastapi/main.py
+++ csv_fastapi/main.py
@@ -1,11 +1,34 @@
 from fastapi import FastAPI
 import uvicorn
+import pandas as pd
 
 app = FastAPI()
 
 @app.get("/")
 async def home():
     return {"message": "Welcome to CSV TO FASTAPI APPLICATION."}
+
+@app.get("/get-csv-data")
+def get_csv_data():
+
+    try:
+        df = pd.read_csv("students_complete.csv")
+
+        return {
+            "columns": list(df.columns),
+            "total_rows": len(df),
+            "data": df.fillna("").to_dict(orient="records")
+        }
+
+    except FileNotFoundError:
+        return {
+            "error": "students_complete.csv not found.(it should be in the working directory)"
+        }
+
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
 
 if __name__ == "__main__":
     uvicorn.run(app)
